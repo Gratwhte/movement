78d1b3a3899cbe01ad3ca0205cc5a53259cb3afc
Gratwhte <gratwhte7@gmail.com>
2026-04-23 11:33:01 +0200

    Add files via upload

---
+++ images.js
@@ -0,0 +1,16 @@
+const IMAGE_PATHS = {
+  logo: "imgs/logo.png",
+  hero: "imgs/hero.png",
+
+  barbers: {
+    sanyi: "imgs/sanyi.png",
+    roli: "imgs/roli.png",
+    mate: "imgs/mate.png"
+  },
+
+  gallery: [
+    "imgs/model1.png",
+    "imgs/model2.png",
+    "imgs/model3.png"
+  ]
+};
